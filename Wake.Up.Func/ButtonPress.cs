da1fa2d6618b50ce1ce9c0ae4d2b7d87df1dc45f
Michael Bonetti <mbonett@duqlight.com>
2018-05-08 15:51:11 -0400

    Created ButtonPress Function

--- Wake.Up.Func/ButtonPress.cs
+++ Wake.Up.Func/ButtonPress.cs
@@ -5,6 +5,7 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventHubs;
 using System.Text;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Wake.Up.Func
 {
@@ -13,9 +14,9 @@ namespace Wake.Up.Func
         private static HttpClient client = new HttpClient();
 
         [FunctionName("ButtonPress")]
-        public static void Run([IoTHubTrigger("messages/events", Connection = "IoTHubConnectionString")]EventData message, TraceWriter log)
+        public static async Task Run([IoTHubTrigger("messages/events", Connection = "IoTHubConnectionString")]EventData message, TraceWriter log)
         {
-            log.Info($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
+            await client.PostAsync("https://prod-07.eastus.logic.azure.com:443/workflows/e6bb3915f050498e81b6f9e26e48aef4/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=rsZc2iVqZij_CsO4ZeMWRvjByX6_Dibp7PqXHtJQkOo", null);
         }
     }
 }
